7594c7e9c73c2ff48b178c415dc07c22311673e9
Henzii <80477931+Henzii@users.noreply.github.com>
2021-12-02 14:22:33 +0200

    Cypress jumittuu...

--- cypress/integration/front_page_loads.js
+++ cypress/integration/front_page_loads.js
@@ -1,15 +1,15 @@
-describe('Etusivu ', function() {
-    it('Etusivu aukeaa', function() {
-      cy.visit('http://localhost:8080')
-      cy.contains('Risbeegomfkerho')
-      // Ei kirjautunut sisään
-      cy.contains('Kirjaudu sisään')
-      cy.contains('Stats').should('not.exist')
-    })
-    it('Sisäänkirjautuminen epäonnistuu väärällä passulla', function () {
-        cy.get('input[name="tunnus"]').first().type('Tester')
-        cy.get('input[name="password"]').first().type("'ölfkewölfe")
-        cy.get('button').contains('Kirjaudu').click()
-        cy.contains('Väärä tunnus tai salasana')
-    })
+describe('Etusivu ', function () {
+  it('Etusivu aukeaa', function () {
+    cy.visit('http://localhost:8080/')
+    cy.contains('Risbeegomfkerho')
+    // Ei kirjautunut sisään
+    cy.contains('Kirjaudu sisään')
+    cy.contains('Stats').should('not.exist')
   })
+  it('Sisäänkirjautuminen epäonnistuu väärällä passulla', function () {
+    cy.get('input[name="tunnus"]').first().type('Tester')
+    cy.get('input[name="password"]').first().type("'ölfkewölfe")
+    cy.get('button').contains('Kirjaudu').click()
+    cy.contains('Väärä tunnus tai salasana')
+  })
+})
